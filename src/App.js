213bf733a14d1394b71394df615374098f9e722f
Lawrence AbuDubai <EstarRave203@gmail.com>
2021-09-26 18:42:21 +0300

    lab11

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import Header from './Header';
 import IsLoadingAndError from './IsLoadingAndError';
+import './App.css';
 import Footer from './Footer';
+import { withAuth0 } from '@auth0/auth0-react';
+import Login from './Login';
+import Profile from './Profile'
+import BestBook from './BestBook'
 import {
   BrowserRouter as Router,
   Switch,
@@ -11,24 +16,26 @@ import {
 class App extends React.Component {
 
   render() {
-    console.log('app', this.props);
-    return(
+    console.log('app', this.props)
+    return (
       <>
         <Router>
           {/* <IsLoadingAndError> */}
             <Header />
             <Switch>
               <Route exact path="/">
-                {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
+                {this.props.auth0.isAuthenticated ? <BestBook /> : <Login />}
               </Route>
-              {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+              <Route exact path="/profile">
+                {this.props.auth0.isAuthenticated && <Profile />}
+              </Route>
             </Switch>
             <Footer />
           {/* </IsLoadingAndError> */}
         </Router>
       </>
-    );
+    )
   }
 }
 
-export default App;
+export default withAuth0(App);
--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,19 +1,27 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
 import Jumbotron from 'react-bootstrap/Jumbotron';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import BestBooks from './BestBook';
+
 import './BestBooks.css';
 
-class MyFavoriteBooks extends React.Component {
+class BestBooks extends React.Component {
   render() {
-    return(
-      <Jumbotron>
-        <h1>My Favorite Books</h1>
-        <p>
-          This is a collection of my favorite books
-        </p>
-      </Jumbotron>
+    return (
+      <>
+        <Container>
+          <Jumbotron>
+            <h1 id="bestbooks" class="text-center">best books</h1>
+            <p id="collection" class="text-center">
+             best books
+            </p>
+          </Jumbotron>
+          <BestBooks />
+        </Container>
+      </>
     )
   }
 }
 
-export default MyFavoriteBooks;
+export default BestBooks;
